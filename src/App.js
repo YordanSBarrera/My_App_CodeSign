0ffd2b43e37474160ba24cdefd7c345b2abf44c5
Yordan SB <yordansaldanabarrera@gmail.com>
2021-09-28 19:17:49 -0300

    INICIALIZANDO EL PROYECTO

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DashboardLayout from "./components/DashboardLayout";
+
+function App() {
+  return (
+    <div className="App">
+      Hola Mundo
+      <br />
+      <DashboardLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DashboardLayout.js
@@ -0,0 +1,5 @@
+const DashboardLayout = () => {
+  return "DASHBOARD LAYOUT";
+};
+
+export default DashboardLayout;
